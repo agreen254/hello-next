637f6dd992e6901f362f6b3500ec5a9922981805
Aaron Green <aarongreen1997@gmail.com>
2023-09-18 15:14:35 -0500

    handle 404 pages

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 
 const Loading = () => {
-  
   return (
     <div>
-        <span>Loading</span><span className="loading loading-dots loading-md"></span>;
+      <span>Loading</span>
+      <span className="loading loading-spinner loading-md"></span>;
     </div>
-  )
+  );
 };
 
 export default Loading;
