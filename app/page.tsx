6d932bbbf7f3f9d33bc3efdc04cf721fb417e6c8
Aaron Green <aarongreen1997@gmail.com>
2023-09-25 12:15:14 -0500

    add assets

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import { authOptions } from "./api/auth/[...nextauth]/route";
 import { getServerSession } from "next-auth";
+import Image from "next/image"; // use this instead of the standard img ele
+import therma from "@/public/images/thermae.jpeg";
 import ProductCard from "./components/ProductCard";
 import HeavyComponentWrapper from "./components/HeavyComponentWrapper";
 import LodashLoader from "./components/LodashLoader";
@@ -8,11 +10,27 @@ export default async function Home() {
   const session = await getServerSession(authOptions);
 
   return (
-    <main>
+    <main className="relative h-screen">
       <h1>Hello, {session && <span>{session.user!.name}</span>}</h1>
       <ProductCard />
       <HeavyComponentWrapper />
       <LodashLoader />
+      <Image src={therma} alt="the baths" />
+      <Image
+        src="https://bit.ly/react-cover"
+        alt="react course logo"
+        width={300}
+        height={170}
+      />
+      {/* <Image
+        src="https://bit.ly/react-cover"
+        alt="filled react logo"
+        fill
+        className="object-cover"
+        sizes="(max-width: 480px) 100vw, (max-width: 768px) 50vw, 33vw"
+        quality={100}
+        priority={false}
+      /> */}
     </main>
   );
 }
